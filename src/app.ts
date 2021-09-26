581cf3d79c6f751bbb7afbe887c099069e286e41
Alireza Nasseh <alireza.nasseh@gmail.com>
2021-09-26 15:38:34 +0330

    Change double quotes to single quotes

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
-import "reflect-metadata";
-import express from "express";
-import config from "./config";
-import dayjs from "dayjs";
+import 'reflect-metadata';
+import express from 'express';
+import config from './config';
+import dayjs from 'dayjs';
 
 async function startServer() {
   const app = express();
 
-  await require("./loaders").default({ expressApp: app });
+  await require('./loaders').default({ expressApp: app });
 
   app
     .listen(config.port, () => {
       console.log(
         `Server is running on port: ${config.port}, Today is ${dayjs(
           new Date()
-        ).format("YYYY:MM:DD")}`
+        ).format('YYYY:MM:DD')}`
       );
     })
-    .on("error", () => {
+    .on('error', () => {
       process.exit(1);
     });
 }
