0c003f14d0417cc582fb1d8684f50012722ec7f9
Alireza <alireza.nasseh@gmail.com>
2021-09-27 12:56:00 +0330

    Get only options, process and return it

--- src/utils/paginate.ts
+++ src/utils/paginate.ts
@@ -1,24 +1,24 @@
 import {QueryOptions} from 'mongoose';
-import {IReadProps} from '../interfaces';
+import {IOptions} from '../interfaces';
 
-export default function Paginate(props: IReadProps<any>): Required<IReadProps<any>> {
+export default function Paginate(options: IOptions | undefined): QueryOptions {
     try {
-        const {
-            filter = {},
-            projection = '',
-            options = {}
-        } = props;
-
         let queryOptions: QueryOptions = {};
-        if (options.sort) queryOptions.sort = options.sort;
-        if (!options.page) options.page = 1;
-        if (!options.limit) options.limit = 25;
-        queryOptions.limit = options.limit;
-        queryOptions.skip = (options.page - 1) * options.limit;
-
-        return {filter, projection, options: queryOptions};
+        if (options) {
+            if (options.sort) queryOptions.sort = options.sort;
+            if (!options.page) options.page = 1;
+            if (!options.limit) options.limit = 25;
+            queryOptions.limit = options.limit;
+            queryOptions.skip = (options.page - 1) * options.limit;
+        } else {
+            queryOptions = {
+                sort: '-_id',
+                limit: 25,
+                skip: 0
+            };
+        }
+        return queryOptions;
     } catch (e) {
         throw e;
     }
 }
-
