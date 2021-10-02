280bee84f94a266180f18dbc30f1810294927841
Alireza <alireza.nasseh@gmail.com>
2021-10-03 00:04:33 +0330

    Paginate by Options util, fixed getOne filter bug by JSON stringify it

--- src/controllers/general.controller.ts
+++ src/controllers/general.controller.ts
@@ -1,8 +1,7 @@
 import {NextFunction, Request, Response} from 'express';
-import {sendResponse} from '../utils';
+import {Options, sendResponse} from '../utils';
 import {GeneralService} from '../services';
 import * as mongoose from 'mongoose';
-import {IOptions} from '../interfaces';
 
 export const create = async <T>(req: Request, res: Response, next: NextFunction, model: mongoose.Model<T>) => {
     try {
@@ -30,20 +29,12 @@ export const getMany = async <T>(req: Request, res: Response, next: NextFunction
             locale: req.locale
         });
 
-        const options: IOptions = {};
-        if (req.query.sort) {
-            options.sort = req.query.sort as string;
-        }
-        if (req.query.pageSize) {
-            options.limit = parseInt(req.query.pageSize.toString());
-        }
-        if (req.query.page) {
-            options.page = parseInt(req.query.page.toString());
-        }
+        const options = Options(req);
         let filter = '{}';
         if (req.query.conditions) {
             filter = req.query.conditions.toString();
         }
+
         const response = await US.getMany({
             filter,
             projection: (req.query.fields ?? '').toString(),
@@ -67,7 +58,7 @@ export const getOne = async <T>(req: Request, res: Response, next: NextFunction,
             locale: req.locale
         });
         const response = await US.getOne({
-            filter: '{_id: req.params.id}',
+            filter: JSON.stringify({_id: req.params.id}),
             projection: (req.query.fields ?? '').toString()
         });
         sendResponse({
