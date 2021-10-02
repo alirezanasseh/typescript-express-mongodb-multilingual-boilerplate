74f8c0f996a969804b5923fe6f436afad2f571a8
Alireza <alireza.nasseh@gmail.com>
2021-10-03 00:05:55 +0330

    Removed Paginate util, it was replaced by Options

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,13 @@
 import sendResponse from './sendResponse';
-import Paginate from './paginate';
 import Normalize from './normalize';
 import AccessControl from './accessControl';
 import Multilingual from './multilingual';
+import Options from "./options";
 
 export {
     sendResponse,
-    Paginate,
     Normalize,
     AccessControl,
-    Multilingual
+    Multilingual,
+    Options
 }
