a66dae69068505b52b4896f3408869e5fd09e63a
Alireza <alireza.nasseh@gmail.com>
2021-10-23 12:59:27 +0330

    Moved multilingualFields to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,11 +3,13 @@ import Normalize from './normalize';
 import AccessControl from './accessControl';
 import Multilingual from './multilingual';
 import Options from "./options";
+import MultilingualFields from "./multilingualFields";
 
 export {
     sendResponse,
     Normalize,
     AccessControl,
     Multilingual,
-    Options
+    Options,
+    MultilingualFields
 }
