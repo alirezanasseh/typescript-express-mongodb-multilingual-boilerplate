ec91d20906fb237f8c633c25962d1116adeaf286
Alireza <alireza.nasseh@gmail.com>
2021-11-21 18:51:42 +0330

    Changed auth service from class to function, added changePassword service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,53 +1,71 @@
 import {User} from '../models';
 import {TokenService} from './index';
 import {
-    IRole,
-    IAuth,
     IUserDTO,
+    IUser,
+    IUserRole
+} from '../interfaces/project';
+import {
+    IAuthRegister,
+    IAuthLogin,
     IService
-} from '../interfaces';
+} from "../interfaces/system";
 
-export default class AuthService {
-    public async register (userDTO: IUserDTO, role: IRole): Promise<IService<IAuth>> {
-        try {
-            const isEmailTaken = await User.isEmailTaken(userDTO.email);
-            if (isEmailTaken) {
-                return {error: 'email_taken'};
-            }
-            const user = await User.create({...userDTO, role});
-            if (user) {
-                const TS = new TokenService();
-                const token = TS.generateToken(user);
-                return {result: {user, token}};
-            } else {
-                return {error: 'register_error'};
-            }
-        } catch (e) {
-            throw e;
+export async function register (userDTO: IUserDTO, role: IUserRole): Promise<IService<IAuthRegister>> {
+    try {
+        const isEmailTaken = await User.isEmailTaken(userDTO.email);
+        if (isEmailTaken) {
+            return {error: 'email_taken'};
         }
+        const user = await User.create({...userDTO, role});
+        if (user) {
+            const token = TokenService.generateToken(user);
+            return {result: {id: user._id.toString(), token}};
+        } else {
+            return {error: 'register_error'};
+        }
+    } catch (e) {
+        throw e;
     }
+}
 
-    public async login (email: string, password: string, role: IRole): Promise<IService<IAuth>> {
-        try {
-            const user = await User.findOne({email});
-            if (user) {
-                if (await user.isPasswordMatch(password)) {
-                    // Checking role
-                    if (user.role === role) {
-                        const TS = new TokenService();
-                        const token = TS.generateToken(user);
-                        return {result: {user, token}};
-                    } else {
-                        return {error: 'incorrect_role'};
-                    }
+export async function login (email: string, password: string, role: IUserRole): Promise<IService<IAuthLogin>> {
+    try {
+        const user = await User.findOne({email});
+        if (user) {
+            if (await user.isPasswordMatch(password)) {
+                // Checking role
+                if (user.role === role) {
+                    const token = TokenService.generateToken(user);
+                    let returnUser: Partial<IUser> = {...user.toJSON()};
+                    delete returnUser.password;
+                    delete returnUser.salt;
+                    return {result: {item: returnUser, token}};
                 } else {
-                    return {error: 'incorrect_password'};
+                    return {error: 'incorrect_role'};
                 }
             } else {
-                return {error: 'email_not_exists'};
+                return {error: 'incorrect_password'};
             }
-        } catch (e) {
-            throw e;
+        } else {
+            return {error: 'email_not_exists'};
         }
+    } catch (e) {
+        throw e;
+    }
+}
+
+export async function changePassword (currentUser: IUser, currentPassword: string, newPassword: string): Promise<IService<object>> {
+    try {
+        let user = await User.findById(currentUser._id);
+        if (user && await user.isPasswordMatch(currentPassword)) {
+            user.password = newPassword;
+            await user.save();
+            return {result: {done: true}};
+        } else {
+            return {error: 'incorrect_password'};
+        }
+    } catch (e) {
+        throw e;
     }
 }
