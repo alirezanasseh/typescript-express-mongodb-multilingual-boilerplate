6680eb87c7c92ae1a4e57d71d4a5722453aaf8e2
Alireza <alireza.nasseh@gmail.com>
2021-11-21 19:01:07 +0330

    Changed from class to function

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -1,21 +1,19 @@
-import {IUser} from '../interfaces';
+import {IUser} from '../interfaces/project';
 import config from '../config';
 import jwt from 'jsonwebtoken';
 
-export default class TokenService {
-    public generateToken(user: IUser) {
-        // Set token expiration date to 60 days
-        const today = new Date();
-        const exp = new Date(today);
-        exp.setDate(today.getDate() + 60);
+export function generateToken(user: IUser) {
+    // Set token expiration date to 60 days
+    const today = new Date();
+    const exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
 
-        return jwt.sign({
-            _id: user._id,
-            role: user.role,
-            full_name: (user.name + ' ' + user.family).trim(),
-            exp: exp.getTime() / 1000
-        }, config.jwtSecret, {
-            algorithm: 'RS256'
-        });
-    }
+    return jwt.sign({
+        _id: user._id,
+        role: user.role,
+        name: user.name.trim(),
+        exp: exp.getTime() / 1000
+    }, config.jwtSecret, {
+        algorithm: 'RS256'
+    });
 }
