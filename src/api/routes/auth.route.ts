0520204214a6c25ad8723c9891f120868515ee1e
Alireza <alireza.nasseh@gmail.com>
2021-11-21 18:50:43 +0330

    Added check and changePassword routes

--- src/api/routes/auth.route.ts
+++ src/api/routes/auth.route.ts
@@ -2,6 +2,8 @@ import {Router} from 'express';
 import {celebrate} from 'celebrate';
 import * as Validator from '../../validators';
 import * as Controller from '../../controllers/auth.controller';
+import authCheck from '../middlewares/authCheck';
+import attachCurrentUser from '../middlewares/attachCurrentUser';
 
 const auth = (app: Router) => {
     const route = Router();
@@ -11,6 +13,8 @@ const auth = (app: Router) => {
     route.post('/login', celebrate(Validator.Login), Controller.login);
     route.post('/admin_login', celebrate(Validator.Login), Controller.admin_login);
     route.post('/logout', Controller.logout);
+    route.post('/check', authCheck, Controller.check);
+    route.post('/change_password', authCheck, attachCurrentUser, celebrate(Validator.ChangePassword), Controller.changePassword);
 };
 
 export default auth;
