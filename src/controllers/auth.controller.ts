089eda432c4b476670818c60f3920b0de413cc34
Alireza <alireza.nasseh@gmail.com>
2021-11-21 18:50:29 +0330

    Added check and changePassword methods

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,11 +2,11 @@ import {NextFunction, Request, Response} from 'express';
 import {sendResponse} from '../utils';
 import {AuthService} from '../services';
 import httpStatus from 'http-status';
+import {IService} from '../interfaces/system';
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const AS = new AuthService();
-        const response = await AS.register(req.body, 'user');
+        const response = await AuthService.register(req.body, 'user');
         sendResponse({res, response, locale: req.locale, token: response.result?.token});
     } catch (e) {
         return next(e);
@@ -15,8 +15,7 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const AS = new AuthService();
-        const response = await AS.login(req.body.email, req.body.password, 'user');
+        const response = await AuthService.login(req.body.email, req.body.password, 'user');
         sendResponse({res, response, locale: req.locale, token: response.result?.token})
     } catch (e) {
         return next(e);
@@ -25,8 +24,7 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
 
 export const admin_login = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const AS = new AuthService();
-        const response = await AS.login(req.body.email, req.body.password, 'admin');
+        const response = await AuthService.login(req.body.email, req.body.password, 'admin');
         sendResponse({res, response, locale: req.locale, token: response.result?.token})
     } catch (e) {
         return next(e);
@@ -37,6 +35,38 @@ export const logout = (req: Request, res: Response, next: NextFunction) => {
     try {
         res.clearCookie('token').sendStatus(httpStatus.OK);
     } catch (e) {
+        return next(e);
+    }
+};
+
+export const check = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let response: IService<any>;
+
+        if (req.token) {
+            response = {result: {ok: true}};
+        } else {
+            response = {error: 'access_denied'};
+        }
+
+        sendResponse({res, response, locale: req.locale});
+    } catch (e) {
+        return next(e);
+    }
+};
+
+export const changePassword = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let response: IService<any>;
+
+        if (req.currentUser) {
+            response = await AuthService.changePassword(req.currentUser, req.body.current, req.body.new);
+        } else {
+            response = {error: 'access_denied'};
+        }
+
+        sendResponse({res, response, locale: req.locale});
+    } catch(e) {
         return next(e);
     }
 };
