e4f2992818895d30fbe1462bdc95529beeaea2e5
Alireza <alireza.nasseh@gmail.com>
2021-11-21 18:54:24 +0330

    Changed import of token and auth service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,6 @@
-import TokenService from './token.service';
-import AuthService from './auth.service';
 import GeneralService from './general.service';
+import * as TokenService from './token.service';
+import * as AuthService from './auth.service';
 
 export {
     TokenService,
